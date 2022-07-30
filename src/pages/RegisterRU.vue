bf7d3929436260af987bb486cb0cc336c561d4c3
Marc Graciov <marc.graciov@pm.me>
2022-07-30 16:14:08 +0300

    Add register ru

--- src/pages/RegisterRU.vue
+++ src/pages/RegisterRU.vue
@@ -4,32 +4,36 @@
       <img src="/assets/logo.svg" alt="logo" />
       <form action="" name="register" class="form_container">
         <input
-          type="text"
-          name="userName"
-          placeholder="Имя пользователя"
-          v-model="username"
+            type="text"
+            name="userName"
+            placeholder="Имя пользователя"
+            v-model="username"
         />
         <input
-          type="text"
-          name="fullName"
-          placeholder="Полное имя"
-          v-model="fullname"
+            type="text"
+            name="fullName"
+            placeholder="Полное имя"
+            v-model="fullname"
         />
         <input
-          type="password"
-          name="password"
-          placeholder="Пароль"
-          v-model="password"
+            type="password"
+            name="password"
+            placeholder="Пароль"
+            v-model="password"
         />
         <input
-          type="password"
-          name="confirmPassword"
-          placeholder="Подтвердить пароль"
-          v-model="confirmpass"
+            type="password"
+            name="confirmPassword"
+            placeholder="Подтвердить пароль"
+            v-model="confirmpassword"
         />
         <br />
         <div class="buttons">
-          <button name="register" type="submit" @click="startRegister()">
+          <button
+              name="register"
+              type="submit"
+              @click.prevent="startRegister()"
+          >
             Регистрация
           </button>
           <a href="#/ru/login">Вход</a>
@@ -38,7 +42,6 @@
     </div>
   </section>
 </template>
-
 <script>
 import axios from "axios";
 export default {
@@ -53,56 +56,66 @@ export default {
       user: {},
     };
   },
-
   methods: {
-    startRegister() {
+    async startRegister() {
       if (
-        this.username == "" ||
-        this.fullname == "" ||
-        this.password == "" ||
-        this.confirmpassword == ""
+          this.username === "" ||
+          this.fullname === "" ||
+          this.password === "" ||
+          this.confirmpassword === ""
       ) {
         alert("Заполните все пробелы!");
+        return;
       } else if (this.confirmpassword !== this.password) {
         alert("Пароли не совпадают!");
-      }
-    },
-    async register() {
-      const profile = await axios.get("api");
-      this.users = profile.data;
-      const isFound = this.users.some((element) => {
-        if (element.username === this.username) {
-          this.user = element;
-          return true;
-        }
-        return false;
-      });
-      if (isFound) {
-        alert(
-          "Такой логин уже есть! Выберите другое имя пользователя или войдите в систему!"
-        );
+        return;
       } else {
-        const data = { username: this.username, password: this.password };
-        const newuser = await axios.post(/*va fi api,*/ data);
-        this.users.push(data);
-        this.$router.push(`/ro/note/${this.user.id}`);
+        const data = {
+          username: this.username,
+          password: this.password,
+          first_name: this.fullname,
+        };
+        const login_data = {
+          username: this.username,
+          password: this.password,
+        };
+        console.log(data);
+        try {
+          const profile = await axios.post(
+              "https://notes-api.girlsgoit.org/register/",
+              data
+          );
+          const login_response = await axios.post(
+              "https://notes-api.girlsgoit.org/login/",
+              login_data
+          );
+          console.log(profile);
+          console.log(login_response);
+          window.localStorage.setItem(
+              "notes-user-key",
+              login_response.data.token
+          );
+          this.$router.push("/ru/dashboard");
+        } catch (e) {
+          console.error(e);
+          alert(
+              "Такой логин уже есть! Выберите другое имя пользователя или войдите в систему!"
+          );
+        }
       }
     },
   },
 };
 </script>
-
-<style>
+<style scoped>
 body {
   padding: 0;
   margin: 0;
   font-family: "Roboto", sans-serif;
 }
-
 a {
   text-decoration: none;
 }
-
 section {
   height: 100vh;
   background-color: #eff3f6;
@@ -112,17 +125,14 @@ section {
   flex-direction: column;
   text-align: center;
 }
-
 .container {
   background-color: #fff;
   width: 352px;
   height: 473px;
 }
-
 .container > img {
   margin: 25px 0px 9.5px 0px;
 }
-
 form > input {
   margin: 9.5px 0px 9.5px 0px;
   padding-left: 21px;
@@ -132,7 +142,6 @@ form > input {
   border-radius: 5px;
   /* color: #a9a9a9; */
 }
-
 .buttons {
   text-transform: uppercase;
   display: flex;
@@ -140,7 +149,6 @@ form > input {
   align-items: baseline;
   margin-top: 20px;
 }
-
 .buttons > button {
   text-transform: uppercase;
   background-color: #2ea1f8;
@@ -150,7 +158,6 @@ form > input {
   padding: 12px 16px 11px 17px;
   width: 140px;
 }
-
 .buttons > a {
   color: #278df6;
   font-size: 13px;
